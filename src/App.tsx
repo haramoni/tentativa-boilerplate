d0a489284fec203dc6f9e0d16ee38c610aba7b32
Júlia Haramoni <haramoni.julia@gmail.com>
2025-04-28 15:56:42 -0300

    instalando tailwind com sucesso

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export function App() {
-    return <></>;
+    return (
+        <div className="bg-mint-500">
+            <h1 className="text-3xl font-bold underline color-mint-500">Hello world!</h1>
+        </div>
+    );
 }
